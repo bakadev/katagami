41ced683ce0d77e4a6f57a221f721ed285e80e89
Travis Wilson <travis.wilson@integrally.one>
2026-04-24 05:30:59 -0400

    feat(client): wrap app in ThemeProvider

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router";
+import { ThemeProvider } from "~/lib/theme/ThemeProvider";
 import App from "./App";
 import "./styles.css";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <ThemeProvider>
+      <BrowserRouter>
+        <App />
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
 );
